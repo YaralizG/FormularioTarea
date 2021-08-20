cd321fbc6776962708f88a02cdb9d06fc2ce9cdc
yaraliz <ygm641@gmail.com>
2021-08-20 01:41:13 -0500

    Modificando formulario

--- FormularioTrabajo/Controllers/VentasControllers.cs
+++ FormularioTrabajo/Controllers/VentasControllers.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using FormularioTrabajo.Models;
 
 namespace FormularioTrabajo.Controllers
-{
-    
+{    
     public class VentasController: Controller
     {
     public IActionResult Index()
@@ -17,11 +16,9 @@ namespace FormularioTrabajo.Controllers
             double Subtotal=0;
             double Total=0;
             double IGV=1.18;
-
             Subtotal =objHacerVenta.Cantidad*objHacerVenta.PrecioProducto;
             Total = Subtotal*IGV;
-
-            ViewData["Message"] = "El total es: "+ Total;
+            ViewData["Message"] = "TOTAL DE VENTA: "+ Total;
             return View("Index");
         }
     }
