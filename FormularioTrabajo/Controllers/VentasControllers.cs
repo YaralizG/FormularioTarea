90cf49ffbe6d96facd6dd966ddd6dd31d6558a2f
yaraliz <ygm641@gmail.com>
2021-08-20 01:05:03 -0500

    Editando formulario

---
+++ FormularioTrabajo/Controllers/VentasControllers.cs
@@ -0,0 +1,28 @@
+
+using Microsoft.AspNetCore.Mvc;
+using FormularioTrabajo.Models;
+
+namespace FormularioTrabajo.Controllers
+{
+    
+    public class VentasController: Controller
+    {
+    public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HacerVenta(Ventas objHacerVenta)
+        {
+            double Subtotal=0;
+            double Total=0;
+            double IGV=1.18;
+
+            Subtotal =objHacerVenta.Cantidad*objHacerVenta.PrecioProducto;
+            Total = Subtotal*IGV;
+
+            ViewData["Message"] = "El total es: "+ Total;
+            return View("Index");
+        }
+    }
+}
---
+++ FormularioTrabajo/Models/Ventas.cs
@@ -0,0 +1,9 @@
+namespace FormularioTrabajo.Models
+{
+    public class Ventas
+    {        
+        public string NombreCliente{get;set;}
+        public double PrecioProducto{get;set;}
+        public double Cantidad{get;set;}
+    }
+}
